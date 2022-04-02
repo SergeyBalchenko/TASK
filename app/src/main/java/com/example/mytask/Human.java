f286f9de777ce27bcaf94bb036c996e91f6db29d
adil <sergey.balchenko@gmail.com>
2022-04-02 13:01:02 +0300

    first commit

---
+++ app/src/main/java/com/example/mytask/Human.java
@@ -0,0 +1,12 @@
+package com.example.mytask;
+
+class Human {
+
+    void buyBread() {
+        System.out.println("Buy bread");
+    }
+
+    void buyMilk() {
+        System.out.println("Buy Milk");
+    }
+}
