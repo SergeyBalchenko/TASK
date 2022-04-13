70025d8335315400fc538265e17c6c48bdd3b0c2
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-04-13 18:25:43 +0300

    try change

--- app/src/main/java/com/example/mytask/MainActivity.java
+++ app/src/main/java/com/example/mytask/MainActivity.java
@@ -13,12 +13,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //Calculator calculator = new Calculator(1, 5);
-        //calculator.add();
-        //calculator.divide();
+
+        Calculator calculator = new Calculator();
+        calculator.add();
+        calculator.divide();
+        }
     }
-}
+
+        class Calculator {
+            private float add = 1;
+            private float divide = 5;
+
+
+            void calculator() {
+                if( add >= divide ){
+                    System.out.println ( add + " - это число больше" );
+                }else{
+                Log.v(TAG,  number2  + " - это число больше");
+
+                }
+            }
+            void addition() {
+                double a = add + divide;
+                System.out.println( add + "+" + divide + "=" + a);
+            }
+            void subtraction() {
+                double b = add - divide;
+                System.out.println( add + "-" + divide + "=" + b);
+            }
+            void division() {
+                double d = add / divide;
+                System.out.println( add + "/" + divide + "=" + d);
+            }
+            void multiplication() {
+                double c = add * divide;
+                System.out.println( add + "*" + divide + "=" + c);
+            }
+        }
 
 
 
