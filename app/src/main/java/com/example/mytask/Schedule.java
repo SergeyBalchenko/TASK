8b11973956d4e0267f8034906e6630da42d2fadc
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-05-10 21:04:47 +0300

    change classes

--- app/src/main/java/com/example/mytask/Schedule.java
+++ app/src/main/java/com/example/mytask/Schedule.java
@@ -1,84 +1,67 @@
 package com.example.mytask;
-
+import java.util.ArrayList;
 public class Schedule {
-
-
-    private static class University {
-        private String name;
-        private String location;
-        private int age;
-        private String phone;
-
-
-        public University(String name, String location, String phone, int age) {
-            this.name = name;
-            this.location = location;
-            this.phone = phone;
-            this.age = age;
-
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public void setLocation(String location) {
-            this.location = location;
-        }
-
-        public void setPhone(String phone) {
-            this.phone = phone;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-
-        private class ScheduleDay {
-            University mondey = new University("Jhon","big hall","Nokia",32);
-            ArrayList<String> subject = new ArrayList<String>();
-            subjectsList.add(subject);
-
-
-        }
-
-        private class Subject {
-            public String getName() {
-                return name;
-            }
-
-        }
-
-        public class Classroom {
-
-            public String getName() {
-                return  name;
-            }
-
-            public String getLocation() {
-                return location;
-            }
-        }
-
-        public class Teacher {
-            public String getName() {
-                return name;
-            }
-
-            public String getPhone() {
-                return phone;
-            }
-
-            public int getAge() {
-                return age;
-            }
-        }
-    }
-
 }
+public class University {
+    private String name;
 
+    public String getName() {
+        return name;
+    }
+    public University() {
+        this.name = name;
+    }
+}
+private class ScheduleDay {
+    ArrayList<String> subject = new ArrayList<String>();
+            subjectsList.add(subject);
+}
+public class Subject {
+    String name;
+    String location;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+}
+public class Classroom {
+    String name;
+    String location;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
+    public String getLocation() {
+        return location;
+    }
+    }
+    public class Teacher {
+    String name;
+    String phone;
+    int age;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public int getAge() {
+        return age;
+    }
+}
