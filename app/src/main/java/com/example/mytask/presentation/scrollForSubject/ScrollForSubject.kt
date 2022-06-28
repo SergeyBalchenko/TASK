66de7cecef709f69cf6b1cd5494148dccea5c0a3
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-06-28 23:55:48 +0300

    Change frame-> constain and add bottom

--- app/src/main/java/com/example/mytask/presentation/scrollForSubject/ScrollForSubject.kt
+++ app/src/main/java/com/example/mytask/presentation/scrollForSubject/ScrollForSubject.kt
@@ -1,4 +1,4 @@
-package com.example.mytask.presentation.info_subject
+package com.example.mytask.presentation.scrollForSubject
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [InfoSubject.newInstance] factory method to
+ * Use the [ScrollForSubject.newInstance] factory method to
  * create an instance of this fragment.
  */
-class InfoSubject : Fragment() {
+class ScrollForSubject : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class InfoSubject : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_subject, container, false)
+        return inflater.inflate(R.layout.fragment_scroll_for_subject, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class InfoSubject : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment InfoSubject.
+         * @return A new instance of fragment ScrollForSubject.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            InfoSubject().apply {
+            ScrollForSubject().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
