64eb1eaca684503ebde6c5cf5783e8ce409d0608
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-07-15 12:03:54 +0300

    Change styly writing name id and change parameters in bottom, create sizing in dimens file

--- app/src/main/java/com/example/mytask/data/model/DayOfWeek.kt
+++ app/src/main/java/com/example/mytask/data/model/DayOfWeek.kt
@@ -1,12 +1,5 @@
 package com.example.mytask.data.model
 
 public class DayOfWeek {
-//    val id: Int
-//    val day: String
-//
-//    public DayOfWeek(id: Int, day: String){
-//        this.id = id
-//        this.day = day
-//    }
 
 }
