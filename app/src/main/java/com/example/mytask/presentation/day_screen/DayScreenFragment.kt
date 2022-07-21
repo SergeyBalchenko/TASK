7391f089d69c310ce352e4808725005dd4e8bfd6
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-07-21 16:34:06 +0300

    don't commit, rename id what find, rebuild table to liner and other ... think do good work.

--- app/src/main/java/com/example/mytask/presentation/day_screen/DayScreenFragment.kt
+++ app/src/main/java/com/example/mytask/presentation/day_screen/DayScreenFragment.kt
@@ -1,6 +1,31 @@
 package com.example.mytask.presentation.day_screen
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.mytask.databinding.DayScreenFragmentBinding
+import com.example.mytask.databinding.MainScreenBinding
+import com.example.mytask.presentation.main.MainFragment
+
 
 class DayScreenFragment : Fragment() {
+    private lateinit var binding: DayScreenFragmentBinding
 
+    companion object {
+        val TAG = MainFragment::class.simpleName
+
+        fun newInstance(): MainFragment {
+            return MainFragment()
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = DayScreenFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
--- app/src/main/java/com/example/mytask/presentation/viewDayShedule/ViewContainerDayShedule.kt
+++ app/src/main/java/com/example/mytask/presentation/viewDayShedule/ViewContainerDayShedule.kt
@@ -1,5 +1,13 @@
 package com.example.mytask.presentation.viewDayShedule
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.mytask.databinding.MainScreenBinding
+import com.example.mytask.presentation.main.MainFragment
+
 class ViewContainerDayShedule : Fragment() {
+
 
 }
