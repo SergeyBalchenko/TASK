fd6ebad43af130af9c70a31145d07d15ec66bd3b
SergeyBalchenko <89645096+SergeyBalchenko@users.noreply.github.com>
2022-09-01 15:44:17 +0300

    Create class adapter initialized in MainActivity.kt and setting with data Teacher

--- app/src/main/java/com/example/mytask/presentation/MainActivity.kt
+++ app/src/main/java/com/example/mytask/presentation/MainActivity.kt
@@ -1,11 +1,7 @@
 package com.example.mytask.presentation
 
 import android.os.Bundle
-import android.view.View
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
 import com.example.mytask.R
@@ -17,18 +13,20 @@ import com.example.mytask.data.entities.University
 import com.example.mytask.data.entities.relations.ClassroomSubjectCrossRef
 import com.example.mytask.data.entities.relations.TeacherSubjectCrossRef
 import com.example.mytask.databinding.ActivityMainBinding
-import com.example.mytask.presentation.main.MainViewModel
 import com.example.mytask.presentation.welcome.WelcomeFragment
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+   private lateinit var binding: ActivityMainBinding
+   private lateinit var adapter: UsersAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        adapter = UsersAdapter()
 
         supportFragmentManager.beginTransaction()
             .replace(R.id.container, WelcomeFragment.newInstance(), WelcomeFragment.TAG)
